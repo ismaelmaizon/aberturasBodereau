9eb4e0673eebeb81652124801498c07b880082d9
ismaelmaizon <ismael.maizon@gmail.com>
2024-07-19 14:06:15 -0300

    mejorando dashboard

--- front/src/components/dashboard/dashboard.jsx
+++ front/src/components/dashboard/dashboard.jsx
@@ -2,7 +2,7 @@ import { Box, Button, Grid, MenuItem, TextField, Typography } from "@mui/materia
 import { useContext, useEffect, useState } from "react";
 import { MiContexto } from "../context/context";
 import { useNavigate } from "react-router-dom";
-
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
 
@@ -26,34 +26,48 @@ export default function Dashboard () {
         <div>
             {
                 cart.length == 0 ? <Typography> El carrito se encuentra vacio </Typography> : 
-                <Box sx={{ width: '80%', display: 'flex', flexDirection: 'column', margin: 'auto', marginTop: '120px', padding: '15px' }} >
+                <Box sx={{ width: '60%', display: 'flex', flexDirection: 'column', margin: 'auto', marginTop: '120px', padding: '15px' }} border='solid' >
                     <Grid>
-                        <Grid marginBottom={5} >
-                            <Typography>Dashboard</Typography>
-                            
+                        <Grid container  >
+                            <Typography  >Dashboard</Typography>
                         </Grid>
-                        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
+                        <Grid container direction='column' border='solid' padding={2} >
                             {
                             cart.map((el, index)=>{ 
-                                return <Grid item xs={2} sm={4} md={4} key={index} container direction="row" color='blue' padding={3} >
-                                        <Button  variant="contained" >
-                                            <Grid key={index} container direction="column" padding={2}  >
-                                            <Typography paddingBottom={1} alignSelf='flex-start'>
-                                                {el.id} 
-                                            </Typography> 
-                                            <Typography >
-                                                Lugar: {el.lugar} 
-                                            </Typography> 
-                                            <Typography >
-                                                Cant: {el.cantidad} 
-                                            </Typography>
+                                return <Grid item xs={2} sm={4} md={4} key={index} 
+                                            container direction="row" color='grey.300' gap={5}
+                                            border='solid 0px' boxShadow='5px 0px 12px 2px' borderRadius={3} margin={1}
+                                            padding={2}>
+                                            
+                                            <Grid item xs={4} color='black' >
+                                                <Typography paddingBottom={1} alignSelf='flex-start'>
+                                                    Producto:  
+                                                </Typography> 
+                                                <Typography >
+                                                    ID: {el.id} 
+                                                </Typography> 
+                                                <Typography >
+                                                    Lugar: {el.lugar} 
+                                                </Typography> 
+                                                <Typography >
+                                                    Cant: {el.cantidad} 
+                                                </Typography>
                                             </Grid>
-                                        </Button> 
+                                            <Grid item xs={4} color='black'>
+                                                <Typography >
+                                                    SubTotal: 
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item xs={2} color='black' alignSelf='flex-end' >
+                                                <Button startIcon={<DeleteIcon/>} >
+                                                    Delete 
+                                                </Button>
+                                            </Grid>
                                     </Grid>
                             })
                             }
                         </Grid>
-                        <Grid>
+                        <Grid marginTop={2} >
                             Total: 
                         </Grid>
                     </Grid>
